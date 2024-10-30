e42deddd50e2692af73e0e3994a75758f819adda
mtc-codes <82724982+mtc-codes@users.noreply.github.com>
2024-10-30 19:33:07 +0000

    Completed assignment. 100%. Have to remember to not make spelling type mistakes

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,4 +2,63 @@ class SiteController < ApplicationController
   def home
     render({:template => "pages/home"})
   end
+
+  def five_four
+    @rolls = []
+
+    5.times do
+      die = rand(1..4)
+
+      @rolls.push(die)
+    end
+    render({:template => "pages/five_four"})
+  end
+
+  def one_twenty
+    @rolls = []
+
+    1.times do
+      die = rand(1..20)
+
+      @rolls.push(die)
+    end
+    render({:template => "pages/one_twenty"})
+  end
+
+  def two_six
+    @rolls = []
+
+        2.times do
+          die = rand(1..6)
+      
+          @rolls.push(die)
+        end
+    render({:template => "pages/two_six"})
+  end 
+
+  def two_ten
+    @rolls = []
+
+    2.times do
+      die = rand(1..10)
+
+      @rolls.push(die)
+    end
+    render({:template => "pages/two_ten"})
+  end
+
+  def flexible
+    @num_dice = params.fetch("number_of_dice").to_i
+
+    @sides = params.fetch("how_many_sides").to_i
+  
+    @rolls = []
+  
+    @num_dice.times do
+      die = rand(1..@sides)
+  
+      @rolls.push(die)
+    end
+    render({ :template => "pages/flexible"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-get("/", {controller => "site", :action => "home"})
+get("/", {:controller => "site", :action => "home"})
+
+get("/dice/5/4", {:controller => "site", :action => "five_four"})
+
+get("/dice/2/6", {:controller => "site", :action => "two_six"})
+
+get("/dice/1/20", {:controller => "site", :action => "one_twenty"})
+
+get("/dice/2/10", {:controller => "site", :action => "two_ten"})
+
+
+get("/dice/:number_of_dice/:how_many_sides", {:controller => "site", :action => "flexible"})
 end
